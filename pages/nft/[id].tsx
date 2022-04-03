3ea7c98ce76597b9f6067b6f011619b44da3cede
vishalpoonacha <vishalpoonacha007@gmail.com>
2022-04-03 10:39:23 +0530

    commit

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
 
 function NFTDropPage() {
+  // Auth
+  const connectWithMetamask = useMetamask()
+  const address = useAddress()
+  const disconnect = useDisconnect()
+  console.log(address)
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* left */}
@@ -34,11 +40,20 @@ function NFTDropPage() {
             </span>{' '}
             NFT Market Place
           </h1>
-          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base ">
-            Sign In
+          <button
+            onClick={() => (address ? disconnect() : connectWithMetamask())}
+            className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base "
+          >
+            {address ? 'Sign Out' : 'Sign In'}
           </button>
         </header>
         <hr className="my-2 border" />
+        {address && (
+          <p className="text-center text-sm text-rose-400">
+            You're logged in with the wallet {address.substring(0, 5)}...1
+            {address.substring(address.length - 5)}
+          </p>
+        )}
         {/* Content */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
           <img
