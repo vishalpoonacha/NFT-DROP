6d27ae273cedaccf36f64d5705fdb0a75070b0ba
vishalpoonacha <vishalpoonacha007@gmail.com>
2022-07-06 20:08:11 +0530

    configured thirdweb for NFT minting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
   return (
     <div className="">
       <Head>
@@ -12,11 +18,51 @@ const Home: NextPage = () => {
           href="https://cdn-icons-png.flaticon.com/128/6228/6228867.png"
         />
       </Head>
-      <h1 className="text-4xl font-bold text-red-500">
-        Welcome to NFT-Drop Build
+      <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+        The{' '}
+        <span className="font-extrabold underline decoration-pink-600/50">
+          PAPAFAM
+        </span>{' '}
+        NFT Market Place
       </h1>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == 'collection']{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage{
+    asset
+  },
+  previewImage{
+    asset
+  },
+  slug{
+    current
+  },
+    
+    creator->{
+    _id,
+    name,
+    address,
+    slug{
+    current
+  }
+  }
+  }`
+
+  const collections = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      collections,
+    },
+  }
+}
